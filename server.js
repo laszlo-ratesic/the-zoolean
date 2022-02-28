162922cb183a07ccf8ed193cf55a189abdf8f1f9
laszlo-ratesic <keenanrc@hotmail.com>
2022-02-28 08:01:02 -0600

    try something

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { animals } = require('./data/animals');
+const { animals } = require('./data/animals.JSON');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
