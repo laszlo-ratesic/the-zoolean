d0ad00d7dbd9eb442edc28b5523e21654f185438
laszlo-ratesic <keenanrc@hotmail.com>
2022-02-28 10:05:35 -0600

    move animals to top of server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const express = require('express');
 const { animals } = require('./data/animals');
-
+const express = require('express');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 function filterByQuery(query, animalsArray) {
