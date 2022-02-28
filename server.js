393e71a8cd5762269c51b8e7a874687ae2a60f15
laszlo-ratesic <keenanrc@hotmail.com>
2022-02-28 10:00:24 -0600

    still trying things

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
