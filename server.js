4f5bffa22660314108e2cb7a00d5b56ca31c95c6
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-03 09:21:18 -0600

    serve up static html and link front-end to express backend

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const { animals } = require("./data/animals");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// serve static files from the public folder
+app.use(express.static('public'));
 
 function validateAnimal(animal) {
   if (!animal.name || typeof animal.name !== "string") {
@@ -107,6 +110,22 @@ app.post("/api/animals", (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(req.body);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
